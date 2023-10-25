c8626695a8b3295d77054bb8c850bc746994a483
hd <1178258121@qq.com>
2023-10-25 12:18:49 +0800

    init

---
+++ used_book/oauth_server/src/test/java/top/haidong/oauth_server/OauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.haidong.oauth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
